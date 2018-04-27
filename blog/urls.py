a710fb1b0615db460c6a0c261c396d2f0166f4bc
fanxiao2 <921516@qq.com>
2018-04-27 21:37:31 +0800

    覆写列表视图、类别视图，增加代码可维护性

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^article/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),
-    url(r'^category/(?P<pk>[0-9]+)/$', views.category, name='category'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^article/(?P<pk>[0-9]+)/$',views.ArticleDetailView.as_view(), name='detail'),
+    url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.ArchivesView.as_view(), name='archives'),
+    url(r'^category/(?P<pk>[0-9]+)/$', views.CategoryView.as_view(), name='category')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,40 +1,70 @@
 import markdown
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from django.http import HttpResponse # 导入HttpResponse函数
 from comments.forms import CommentForm #导入评论
 from .models import Article, Category
 # Create your views here.
 
-def index(request):
-    article_list = Article.objects.all().order_by('-create_at')
-    return render(request, 'blog/index.html', context={'article_list':article_list})
+class IndexView(ListView):
+    model = Article
+    template_name = 'blog/index.html'
+    context_object_name = 'article_list'
 
 # 实现detail 视图
 
-def detail(request, pk):
-    article = get_object_or_404(Article, pk=pk)
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'blog/detail.html'
+    context_object_name = 'article'
 
-    # 阅读量 +1
-    article.increase_reading()
-    article.content = markdown.markdown(article.content,
-                                        extensions = [
-                                            'markdown.extensions.extra',
-                                            'markdown.extensions.codehilite',
-                                            'markdown.extensions.toc',
-                                        ])
-    form = CommentForm()
-    # 获取文章下对应的全部评论
-    comment_list = article.comment_set.all()
-    # 将文章，表单和文章下面的评论列表当做模板传递给 detail.html 模板并渲染相对应的数据
-    context = {'article':article, 'form':form, 'comment_list':comment_list}
-    return render(request, 'blog/detail.html', context=context)
+    # 重写get方法 目的是实现文章阅读+1
+    def get(self, request, *args, **kwargs):
+        response = super(ArticleDetailView, self).get(request, *args, **kwargs)
+        # 降阅读量加1
+        self.object.increase_reading()
+        # 返回HttpResponse对象
+        return response
+
+    # 覆写get_object方法，对article的content值进行渲染
+    def get_object(self, queryset=None):
+        article = super(ArticleDetailView, self).get_object(queryset=None)
+        article.content = markdown.markdown(article.content,
+                                            extensions = [
+                                                'markdown.extensions.extra',
+                                                'markdown.extensions.codehilite',
+                                                'markdown.extensions.toc',
+                                            ])
+        return article
+    # 覆写get_content_date 降article 和评论表单以及article的评论列表传递给视图模板
+    def get_context_data(self, **kwargs):
+        context = super(ArticleDetailView, self).get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+            'form':form,
+            'comment_list':comment_list
+        })
+        return context
 
 # 归档
-def archives(request, year, month):
-    article_list = Article.objects.filter(create_at__year=year,create_at__month=month).order_by('-create_at')
-    return render(request, 'blog/index.html', context={'article_list':article_list})
+class ArchivesView(ListView):
+    model = Article
+    template_name = 'blog/index.html'
+    context_object_name = 'article_list'
+
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super(ArchivesView, self).get_queryset().filter(create_at__year, create_at__month)
+
+        
 # 分类
-def category(request,pk):
-    cate = get_object_or_404(Category,pk=pk)
-    article_list = Article.objects.filter(category=cate).order_by('-create_at')
-    return render(request, 'blog/index.html', context={'article_list':article_list})
+class CategoryView(ListView):
+    model = Article
+    template_name = 'blog/index.html'
+    context_object_name = 'article_list'
+
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
